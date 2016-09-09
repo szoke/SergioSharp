9d436618cb1c99164fbca33446b0431100003ebb
Andras Szoke <v-aszoke@expedia.com>
2016-09-09 10:18:28 +0200

    Add array examples

--- SergioSharp/Program.cs
+++ SergioSharp/Program.cs
@@ -13,6 +13,7 @@ namespace SergioSharp
             DoFunStuffWithStrings();
             DoFunStuffWithIntegers();
             DoFunStuffWithFloats();
+            DoFunStuffWithArrays();
         }
 
         private static void DoFunStuffWithStrings()
@@ -71,6 +72,42 @@ namespace SergioSharp
             Console.WriteLine(Math.Sqrt(a));
 
             // TODO: Megnézni a többi metódust a Math osztályban. Elkezded írni, hogy 'Math.' és a Visual Studio feldobja a lehetőségeket.
+
+            Console.ReadLine();
+        }
+
+        private static void DoFunStuffWithArrays()
+        {
+            // A tömbökben több ugyanolyan típusú elemet lehet tárolni
+
+            // Csináljunk egy 3 elemű tömböt
+            int[] ints = new int[3];
+
+            // Töltsük fel
+            // A tömbelemeket indexeléssel érjük el, az indexek 0-tól kezdődnek
+            // Az utolsó elem indexe ezért length - 1, itt 3 - 1 = 2
+            ints[0] = 45;
+            ints[1] = 46;
+            ints[2] = 47;
+
+            // for ciklussal végigmegyünk a tömb összes elemén és kiiratjuk őket
+            for (int i = 0; i < ints.Length; i++)
+            {
+                Console.WriteLine("A tömb " + i + ". eleme: " + ints[i]);
+            }
+
+            // Tömb létrehozása az elemek felsorolásával
+            int[] intsTwo = new int[] { 5, 8, 4, 7, 8, 9, 4, 1, 2, 4, 5, 6, 5, 4 };
+            Console.WriteLine(intsTwo.Length + " darab elem van a felsorolással létrehozott tömbben.");
+
+            // for ciklussal kiiratjuk az utolsó 3 elemet
+            int indexOfLastElement = intsTwo.Length - 1;
+            for (int i = indexOfLastElement; i > indexOfLastElement - 3; i--)
+            {
+                Console.WriteLine("A tömb " + i + ". eleme: " + intsTwo[i]);
+            }
+
+            // TODO: játszani for ciklusokkal
 
             Console.ReadLine();
         }
